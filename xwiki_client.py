7d997ed62e46eb2b0477c393a3d1cd61f3739ec3
Vladimir Anianov <v.s.anyanov@gmail.com>
2026-02-19 07:59:51 +0300

    Update xwiki_client.py

--- xwiki_client.py
+++ xwiki_client.py
@@ -18,7 +18,7 @@ XWIKI_PASSWORD = os.getenv("XWIKI_PASSWORD", "")
 
 XWIKI_EXPORT_URL = os.getenv(
     "XWIKI_EXPORT_URL",
-    "http://158.255.1.153/bin/export/Tech/CorpGPTExporter/WebHome?xpage=plain",
+    "http://158.255.1.153/bin/view/Tech/CorpGPTExporter?xpage=plain",
 )
 
 # ========= CHUNKING =========
